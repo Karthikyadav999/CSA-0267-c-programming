504c5f64dffaf25a40c80d2d89211c904a65d20d
Karthikyadav999 <123459748+Karthikyadav999@users.noreply.github.com>
2023-04-06 09:10:29 +0530

    Add files via upload

---
+++ class percentage,avg,fail.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int phy, chem, bio, math, comp; 
+    float per; 
+
+    
+    printf("Enter four subjects marks: ");
+    scanf("%d%d%d%d", &phy, &chem, &bio, &math);
+
+    per = (phy + chem + bio + math) / 4.0;
+
+    printf("Percentage = %.2f\n", per);
+    
+    if(per >= 90)
+    {
+        printf("Grade A");
+    }
+    else if(per >= 80)
+    {
+        printf("Grade B");
+    }
+    else if(per >= 70)
+    {
+        printf("Grade C");
+    }
+    else if(per >= 60)
+    {
+        printf("Grade D");
+    }
+    else if(per >= 40)
+    {
+        printf("Grade E");
+    }
+    else
+    {
+        printf("Grade F");
+    }
+}
